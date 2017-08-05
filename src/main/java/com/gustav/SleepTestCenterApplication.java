233442db4c31e4893062c71ce91eb7df1e38eaf4
刘鸿城 <gustav@liuhongchengdeMacBook-Pro.local>
2017-08-05 16:14:26 +0800

    Initial commit

---
+++ src/main/java/com/gustav/SleepTestCenterApplication.java
@@ -0,0 +1,12 @@
+package com.gustav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepTestCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepTestCenterApplication.class, args);
+	}
+}
